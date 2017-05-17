d169f4c76ece7e8bfc0a32acfc2c078ad7451a6d
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-17 19:25:02 +0300

    Create new tasks in the do to list

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'slim'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
 get '/' do
+  @tasks = Task.all
   slim :index
 end
 
@@ -9,3 +13,16 @@ get '/:task' do
   @task = params[:task].split('-').join(' ').capitalize
   slim :task
 end
+
+post '/' do
+  Task.create(name: params[:task])
+  redirect to('/')
+end
+
+class Task
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :completed_at, DateTime
+end
+DataMapper.finalize
