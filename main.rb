d5d1ee09017f709455de34ecd7268f21f50acd85
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-18 14:30:09 +0300

    Add too many features in one commit

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'data_mapper'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
 get '/' do
-  @tasks = Task.all
+  @lists = List.all(:order => [:name])
   slim :index
 end
 
@@ -15,7 +15,29 @@ get '/:task' do
 end
 
 post '/' do
-  Task.create(name: params[:task])
+  Task.create params[:task]
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
+end
+
+post '/new/list' do
+  List.create params['list']
+  redirect to('/')
+end
+
+delete '/list/:id' do
+  List.get(params[:id]).destroy
   redirect to('/')
 end
 
@@ -24,5 +46,14 @@ class Task
   property :id,           Serial
   property :name,         String, :required => true
   property :completed_at, DateTime
+  belongs_to :list
 end
+
+class List
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  has n, :tasks, :constraint => :destroy
+end
+
 DataMapper.finalize
